d5eb6f7ae473a8cd9f7c67525d13cdbcf147bacb
ZTz <36158219+ZTzTopia@users.noreply.github.com>
2021-10-20 14:00:12 +0700

    Delete app/src/main/obj directory

--- app/src/main/obj/local/armeabi-v7a/objs/ModMenu/src/utilities/Logging.o.d
+++
@@ -1,5 +0,0 @@
-D:/Project/GTInternalAndroid/app/src/main/obj/local/armeabi-v7a/objs/ModMenu/src/utilities/Logging.o: \
-  D:\Project\GTInternalAndroid\app\src\main\jni\src\utilities\Logging.cpp \
-  D:\Project\GTInternalAndroid\app\src\main\jni\src\utilities\Logging.h
-
-D:\Project\GTInternalAndroid\app\src\main\jni\src\utilities\Logging.h:
